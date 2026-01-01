c7d5374cc23009c9f0e99fede668f0341e517fc3
manseebhossain1 <manseebhossain1@gmail.com>
2026-01-01 18:33:41 -0500

    javascript dropdown made

---
+++ languages.js
@@ -0,0 +1,23 @@
+// A practical dropdown list. You can expand/replace this with a dataset if your course provides one.
+window.LANGUAGES = [
+  "JavaScript",
+  "TypeScript",
+  "Python",
+  "Java",
+  "C#",
+  "C++",
+  "C",
+  "Go",
+  "Rust",
+  "Ruby",
+  "PHP",
+  "Kotlin",
+  "Swift",
+  "R",
+  "Dart",
+  "Scala",
+  "Lua",
+  "Shell",
+  "HTML",
+  "CSS"
+];
